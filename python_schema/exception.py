942b0c5cc2152150a01d65bd70682c3f5b197fac
Darius Drachenfels Viatrak <dariusz.wiatrak@essenceglobal.com>
2018-12-02 23:30:09 +0000

    Reform exceptions, remove old one, rename current ones.

--- python_schema/exception.py
+++ python_schema/exception.py
@@ -1,79 +1,30 @@
 import json
 
 
-class BasePythonSchemaException(Exception):
-    pass
+class BasePythonSchemaError(Exception):
+    """Base class in order to catch all python-schema easily (if needed)
+    """
 
 
-class PythonSchemaException(BasePythonSchemaException):
-    pass
+class ValueNotSetError(BasePythonSchemaError):
+    """Happens when we try to read a field that was never loaded
+    """
 
 
-class InvalidValueError(BasePythonSchemaException):
-    pass
+class NormalisationError(BasePythonSchemaError):
+    """Base error for all the normalisation issues
+    """
 
 
-class ValidationError(BasePythonSchemaException):
-    pass
+class NoneNotAllowedError(NormalisationError):
+    """Happens when we try to load field with None, but it's not allowed
+    """
 
 
-class NotSetError(BasePythonSchemaException):
-    pass
+class ValidationError(BasePythonSchemaError):
+    """Exception happens when one or more of validators fails.
+    """
+    def __init__(self, errors, *args, **kwargs):
+        self.errors = errors
 
-
-# class MultipleValidationError(BaseValidationError):
-#     """Aggregated errors from validatin, used to make a proper response in
-#     human readable form.
-#     """
-#     def __init__(self, schema, errors, *args, **kwargs):
-#         self.errors = errors
-#         self.schema = schema
-#
-#         super().__init__(*args, **kwargs)
-#
-#     def get_errors(self):
-#         """Method returns errors in a format that could be easily consumed by
-#         other parts of the system.
-#
-#         Ie.
-#         {
-#             'username': {
-#                 'error_code': '40-20',
-#                 'errors': [
-#                     'Username cannot be empty',
-#                 ],
-#             },
-#             'address': {
-#                 'error_code': '12-20',
-#                 'errors': {
-#                     'city': {
-#                         'error_code': '25-13',
-#                         'errors': [
-#                             'City is not recognised',
-#                         ]
-#                     },
-#                     'post_code': {
-#                         'error_code': '25-16',
-#                         'errors': [
-#                             'Post code cannot be empty',
-#                         ],
-#                     },
-#                 },
-#             },
-#         }
-#         """
-#         # this import is on purpose to avoid cyclic-imports
-#         from magicarp import tools
-#
-#         tmp = {}
-#
-#         for key, errors in self.errors.items():
-#             tmp[key] = {
-#                 'error_code': tools.helpers.make_error_code(key, self.schema),
-#                 'errors': errors,
-#             }
-#
-#         return tmp
-#
-#     def __str__(self):
-#         return json.dumps(self.get_errors())
+        super().__init__(*args, **kwargs)
