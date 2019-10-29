b9d47198f12bd064aa980deba6406b5d6d27a51c
Drachenfels <drachenfels@protonmail.com>
2019-10-29 16:12:16 +0000

    Rename get_instance into get_class to reflect what it does.

--- python_schema/misc.py
+++ python_schema/misc.py
@@ -10,7 +10,7 @@ class ImportModule:
         self.class_name = path_to_class.split('.')[-1]
         self.path_to_module = path_to_class[:(len(self.class_name) + 1) * -1]
 
-    def get_instance(self):
+    def get_class(self):
         """Methods attempt to import class from module and returns instance of
         the imported class"""
         module = importlib.import_module(self.path_to_module)
